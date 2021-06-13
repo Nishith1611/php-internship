5495145c747b2aa33469d82bb598d63dfeb13199
Patel Nishith Rakeshbhai <85406079+Nishith1611@users.noreply.github.com>
2021-06-13 15:25:53 +0530

    day 7

---
+++ Task7/datatable.php
@@ -0,0 +1,8 @@
+<?php
+$connection= mysqli_connect("localhost","root","","db_internship");
+
+$id=$_Get['deleted'];
+$q= mysqli_query($connection, "delete from tbl_user where user_id='{$id}'")or die(mysqli_error($connection));
+if($q){
+    echo "<script>alert('record deleted');window.location='3table.php'</script>";
+}
